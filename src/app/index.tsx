2fe66f02a2a201332da4b029d2ee6ddd8f2ef4ec
CarlosHGOliveira <carlos.oliveira@e-gestao.com.br>
2024-10-08 14:38:40 -0300

    melhorando tela Sign-In

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -8,23 +8,27 @@ import { StatusBar } from "react-native";
 import { useState } from "react";
 import AntDesign from '@expo/vector-icons/AntDesign';
 import classNames from "classnames";
+import { useRouter } from "expo-router";
+import InputFloatLabel from "@/components/NewInput";
+import SignUp from "./sign-up";
 
 export default function Index() {
   const [signUp, setSignUp] = useState(false);
-
-  function handleSignUp() {
-    setSignUp(!signUp)
-  }
+  const router = useRouter();
 
   return (
-    <View className="flex-1">
+    <View
+      className="flex-1"
+    >
       <ScrollView
         contentContainerStyle={{ flexGrow: 1, justifyContent: 'space-between' }}
         className="bg-background"
         showsVerticalScrollIndicator={false}
         bounces={false}
       >
-        <StatusBar barStyle='light-content' />
+        <StatusBar
+          barStyle='light-content'
+        />
 
         <View className={classNames("p-5 justify-between", {
           'flex-1': !signUp
@@ -44,12 +48,24 @@ export default function Index() {
               Faça login e comece a se conectar com pessoas próximas a você.
             </Text>
 
-            <Input label="E-mail" />
-            <Input label="Senha" password={true} />
-            <Text className="text-base color-white text-right underline">Esqueci minha senha</Text>
+            <Input
+              label="E-mail"
+              templateWhite={true} />
+            <Input
+              label="Senha"
+              password={true}
+              templateWhite={true} />
 
-            <Button title="Entrar" />
+            <Text
+              className="text-base color-white text-right underline"
+            >
+              Esqueci minha senha
+            </Text>
 
+            <Button
+              title="Entrar"
+              onPress={() => router.push('/(tabs)')}
+            />
 
             <View className="flex-row items-center self-center my-5 " style={{ gap: 20 }}>
               <Text className="text-base font-medium color-white">Continuar sem login</Text>
@@ -61,41 +77,33 @@ export default function Index() {
 
 
             <View style={styles.container}>
-              <View style={styles.line} />
-              <Text style={styles.text}>Ou entre com</Text>
-              <View style={styles.line} />
+              <View
+                style={styles.line} />
+              <Text
+                style={styles.text}>
+                Ou entre com
+              </Text>
+              <View
+                style={styles.line} />
             </View>
 
             <View
               className="bg-[#ffffff2b] rounded-lg items-center p-3 w-20 self-center mt-5 mb-3 border-2 border-white"
             >
-              <Linkeding width={42} height={42} />
-            </View></>}
+              <Linkeding
+                width={42}
+                height={42}
+              />
+            </View>
+          </>}
 
 
         </View>
 
-        <View className={classNames("bg-white  rounded-t-[30px] py-10 px-8",
-          {
-            'flex-grow': signUp
-          }
-        )}>
-          <View className="flex-row items-center justify-between">
-            <View>
-              <Text className="text-base font-medium color-[#528A8C]">Novo por aqui?</Text>
-              <Text className='text-3xl font-bold'>Criar conta</Text>
-            </View >
-            <TouchableOpacity
-              className="bg-[#528A8C2b] rounded-3xl items-center p-5 w-20 self-center border-2 border-white"
-              onPress={handleSignUp}
-            >
-              {signUp ? <AntDesign name="close" size={32} color="#528A8C" /> : <Feather name="arrow-up-right" size={32} color="#528A8C" />}
-            </TouchableOpacity>
-          </View >
-        </View >
+        <SignUp signUp={signUp} setSignUp={setSignUp} />
 
-      </ScrollView>
-    </View>
+      </ScrollView >
+    </View >
   );
 }
 
@@ -107,11 +115,11 @@ const styles = StyleSheet.create({
   line: {
     flex: 1,
     height: 1,
-    backgroundColor: '#ffffff', // Cor da linha
+    backgroundColor: '#ffffff',
   },
   text: {
     marginHorizontal: 10,
-    color: '#ffffff', // Cor do texto
+    color: '#ffffff',
     fontSize: 16,
     lineHeight: 25,
     fontWeight: '400',
