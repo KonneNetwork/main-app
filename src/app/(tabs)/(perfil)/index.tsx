7ba6f90bdcf4ef62c9d7a0b51af87ee098e8550c
CarlosHGOliveira <carlos.oliveira@e-gestao.com.br>
2024-10-18 16:55:51 -0300

    Ajustando layout do Perfil

--- src/app/(tabs)/(perfil)/index.tsx
+++ src/app/(tabs)/(perfil)/index.tsx
@@ -33,63 +33,68 @@ function Perfil() {
   return (
     <View className='flex-1'>
       <KeyboardAvoidingView className="flex-1" behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
-        <ScrollView
-          className="bg-surface-primary"
-          contentContainerStyle={{ flexGrow: 1, alignItems: 'center', padding: 30 }}
-          showsVerticalScrollIndicator={false}
+        <FlatList
+          className='bg-surface-primary flex-1'
+          data={addLink}
+          ListHeaderComponentStyle={{ alignItems: 'center' }}
+          ListHeaderComponent={
+            <>
+              <Text className='font-roboto-700 text-xl mt-8'>Meu Perfil</Text>
+
+              <HeaderUser onOpen={handleOpenModalPerfil} />
+
+              <InputPerfil isEditable={false} multiline={true} label='Nome' placeholder='Escreva um texto de apresentação ...' onOpen={handleCloseModalPerfil} />
+
+
+              <View className='w-full gap-5 mt-6'>
+                <Text>Meus links</Text>
+              </View>
+
+            </>
+          }
+          ListFooterComponentStyle={{ marginVertical: 30 }}
+          ListFooterComponent={
+            <>
+              <TouchableOpacity
+                className='flex-row bg-[#F4F4F4] w-full justify-center items-center p-4 gap-3 rounded-lg'
+                onPress={hadleOpenModalLinks}
+              >
+                <Feather name="plus" size={24} color="black" />
+                <Text className='font-roboto-500 text-base'>
+                  Adicionar link
+                </Text>
+              </TouchableOpacity>
+
+
+              <Modal visible={openPerfil} transparent={true} presentationStyle='overFullScreen' animationType='fade' style={{ backgroundColor: '#000', flex: 1 }} >
+                <View style={{ flex: 1, backgroundColor: "#0000002f" }}>
+                  <EditPerfil onClosed={handleCloseModalPerfil} />
+                </View>
+              </Modal>
+              <Modal visible={openLinks} presentationStyle='fullScreen' animationType='fade' style={{ backgroundColor: '#000', flex: 1 }} >
+                <View style={{ flex: 1, backgroundColor: "#0000002f" }}>
+                  <AddLink onClose={handleCloseModalLinks} selectingLinks={setAddLink} selectedLinks={addLink} />
+                </View>
+              </Modal>
+            </>
+          }
+          contentContainerStyle={{
+            justifyContent: 'space-between',
+            paddingHorizontal: 30,
+
+          }}
           bounces={false}
-
-        >
-          <Text className='font-roboto-700 text-xl '>Meu Perfil</Text>
-
-          <HeaderUser onOpen={handleOpenModalPerfil} />
-
-          <InputPerfil isEditable={false} multiline={true} label='Nome' placeholder='Escreva um texto de apresentação ...' onOpen={handleCloseModalPerfil} />
-
-
-          <View className='w-full gap-5 mt-6'>
-            <Text>Meus links</Text>
-            <FlatList
-              data={addLink}
-              contentContainerStyle={{
-                flex: 1,
-                justifyContent: 'space-between',
-              }}
-              bounces={false}
-              showsVerticalScrollIndicator={false}
-              keyExtractor={(item) => item.name}
-              renderItem={({ item }) => (
-                <TouchableOpacity style={[styles.item, { width: itemWidth }]}>
-                  {item.icon}
-                  <Text className='font-roboto-500 text-sm'>{item.name}</Text>
-                </TouchableOpacity>
-              )}
-              numColumns={3}
-            />
-
-            <TouchableOpacity
-              className='flex-row bg-[#F4F4F4] w-full justify-center items-center p-4 gap-3 rounded-lg'
-              onPress={hadleOpenModalLinks}
-            >
-              <Feather name="plus" size={24} color="black" />
-              <Text className='font-roboto-500 text-base'>
-                Adicionar link
-              </Text>
+          showsVerticalScrollIndicator={false}
+          keyExtractor={(item) => item.name}
+          renderItem={({ item }) => (
+            <TouchableOpacity style={[styles.item, { width: itemWidth }]}>
+              {item.icon}
+              <Text className='font-roboto-500 text-sm'>{item.name}</Text>
             </TouchableOpacity>
-            {/* </Link> */}
-          </View>
+          )}
+          numColumns={3}
+        />
 
-          <Modal visible={openPerfil} transparent={true} presentationStyle='overFullScreen' animationType='fade' style={{ backgroundColor: '#000', flex: 1 }} >
-            <View style={{ flex: 1, backgroundColor: "#0000002f" }}>
-              <EditPerfil onClosed={handleCloseModalPerfil} />
-            </View>
-          </Modal>
-          <Modal visible={openLinks} presentationStyle='fullScreen' animationType='fade' style={{ backgroundColor: '#000', flex: 1 }} >
-            <View style={{ flex: 1, backgroundColor: "#0000002f" }}>
-              <AddLink onClose={handleCloseModalLinks} selectingLinks={setAddLink} selectedLinks={addLink} />
-            </View>
-          </Modal>
-        </ScrollView>
       </KeyboardAvoidingView>
     </View >
   )
